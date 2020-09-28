8431b4ea8fa2522728b290895a281ed5c0df801c
DJ <davidabraham@ucla.edu>
2020-09-27 19:43:37 -0700

    data structures added

---
+++ src/intermediate/01-data_structures.jl
@@ -0,0 +1,15 @@
+# Array Containers
+v = Vector{Int64}(undef, 5)
+m = Matrix{Float64}(undef, 3, 4)
+a = Array{Real}(undef, 2, 2, 3)
+
+# Tuple Containers -- immutable
+t = ("hello", 3, 4.5)
+nt = (a="world", b=5, c='a')
+println("keys ", keys(nt))
+println("values ", values(t))
+
+# Dictionary Containers
+d = Dict('a'=>3.4, "hello"=>t[2], 1=>nt.a)
+println("keys ", keys(d))
+println("values ", values(d))
